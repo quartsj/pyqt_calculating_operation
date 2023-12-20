d3ebc1f16303204935b238d7a7984c11c9405637
quartsj <136243732+quartsj@users.noreply.github.com>
2023-12-20 23:55:50 +0900

    Update calculator_main.py

--- calculator_main.py
+++ calculator_main.py
@@ -1,7 +1,6 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QDialog, QVBoxLayout, QHBoxLayout, QGridLayout, QPushButton, QLineEdit, QLabel
 
-
 class Main(QDialog):
     def __init__(self):
         super().__init__()
@@ -10,52 +9,42 @@ class Main(QDialog):
     def init_ui(self):
         main_layout = QVBoxLayout()
 
-        ### 각 위젯을 배치할 레이아웃을 미리 만들어 둠
         layout_operation = QHBoxLayout()
         layout_clear_equal = QHBoxLayout()
         layout_number = QGridLayout()
 
-        ### 수식 입력과 답 출력을 위한 LineEdit 위젯 생성
         self.equation_line_edit = QLineEdit("")
-        self.equation_line_edit.setReadOnly(True)  # Make it read-only to display the result
-        self.equation_line_edit.setAlignment(Qt.AlignRight)
+        self.equation_line_edit.setReadOnly(True)
+        self.equation_line_edit.setAlignment(Qt.AlignRight)  # You may need to import Qt from PyQt5.QtCore
         self.equation_line_edit.setMaxLength(15)
 
-        ### 사칙연산 버튼 생성
         button_plus = QPushButton("+")
         button_minus = QPushButton("-")
         button_product = QPushButton("x")
         button_division = QPushButton("/")
 
-        ### 사칙연산 버튼을 클릭했을 때, 각 사칙연산 부호가 수식창에 추가될 수 있도록 시그널 설정
         button_plus.clicked.connect(lambda state, operation="+": self.button_operation_clicked(operation))
         button_minus.clicked.connect(lambda state, operation="-": self.button_operation_clicked(operation))
         button_product.clicked.connect(lambda state, operation="*": self.button_operation_clicked(operation))
         button_division.clicked.connect(lambda state, operation="/": self.button_operation_clicked(operation))
 
-        ### 사칙연산 버튼을 layout_operation 레이아웃에 추가
         layout_operation.addWidget(button_plus)
         layout_operation.addWidget(button_minus)
         layout_operation.addWidget(button_product)
         layout_operation.addWidget(button_division)
 
-        ### =, clear, backspace 버튼 생성
         button_equal = QPushButton("=")
         button_clear = QPushButton("Clear")
         button_backspace = QPushButton("Backspace")
 
-        ### =, clear, backspace 버튼 클릭 시 시그널 설정
         button_equal.clicked.connect(self.button_equal_clicked)
         button_clear.clicked.connect(self.button_clear_clicked)
         button_backspace.clicked.connect(self.button_backspace_clicked)
 
-        ### =, clear, backspace 버튼을 layout_clear_equal 레이아웃에 추가
         layout_clear_equal.addWidget(button_clear)
         layout_clear_equal.addWidget(button_backspace)
         layout_clear_equal.addWidget(button_equal)
 
-        ### 숫자 버튼 생성하고, layout_number 레이아웃에 추가
-        ### 각 숫자 버튼을 클릭했을 때, 숫자가 수식창에 입력 될 수 있도록 시그널 설정
         number_button_dict = {}
         for number in range(0, 10):
             number_button_dict[number] = QPushButton(str(number))
@@ -66,7 +55,6 @@ class Main(QDialog):
             elif number == 0:
                 layout_number.addWidget(number_button_dict[number], 3, 1)
 
-        ### 소수점 버튼과 00 버튼을 입력하고 시그널 설정
         button_dot = QPushButton(".")
         button_dot.clicked.connect(lambda state, num=".": self.number_button_clicked(num))
         layout_number.addWidget(button_dot, 3, 2)
@@ -75,8 +63,7 @@ class Main(QDialog):
         button_double_zero.clicked.connect(lambda state, num="00": self.number_button_clicked(num))
         layout_number.addWidget(button_double_zero, 3, 0)
 
-        ### 각 레이아웃을 main_layout 레이아웃에 추가
-        main_layout.addWidget(self.equation_line_edit)  # Add equation_line_edit directly to the layout
+        main_layout.addWidget(self.equation_line_edit)
         main_layout.addLayout(layout_operation)
         main_layout.addLayout(layout_clear_equal)
         main_layout.addLayout(layout_number)
@@ -84,9 +71,6 @@ class Main(QDialog):
         self.setLayout(main_layout)
         self.show()
 
-    #################
-    ### functions ###
-    #################
     def number_button_clicked(self, num):
         equation = self.equation_line_edit.text()
         equation += str(num)
@@ -116,5 +100,4 @@ class Main(QDialog):
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     main = Main()
-    main.show()
     sys.exit(app.exec_())
