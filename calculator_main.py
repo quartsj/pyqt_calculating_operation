20dbdf82263ba07e838e486c366c52a95d16478b
quartsj <136243732+quartsj@users.noreply.github.com>
2023-12-20 23:57:04 +0900

    Update calculator_main.py

--- calculator_main.py
+++ calculator_main.py
@@ -1,5 +1,6 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QDialog, QVBoxLayout, QHBoxLayout, QGridLayout, QPushButton, QLineEdit, QLabel
+from PyQt5.QtCore import Qt
 
 class Main(QDialog):
     def __init__(self):
